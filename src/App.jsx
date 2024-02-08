d38e982d4258fb5fe10fe0dee3f77bb7002d48b1
Luca Casamassima <casamassimaluca723@gmail.com>
2024-02-08 18:53:55 +0100

    Fixed not showing projects bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import Content from "./Components/Content";
 import Sidebar from "./Components/Sidebar";
 import "./index.css";
+import Form from "./Components/Form";
 function App() {
+  const [manageProject, setManageProject] = useState({
+    isAdding: undefined,
+    projects: [
+      {
+        title: "Project",
+        description: "my project",
+        date: "24/06/3023",
+        id: "43213",
+      },
+    ],
+  });
+
+  function showForm() {
+    setManageProject({ ...manageProject, isAdding: true });
+  }
+  let showed;
+  if (manageProject.isAdding === true) {
+    showed = <Form />;
+  } else {
+    showed = <Content onAdd={showForm} />;
+  }
   return (
     <>
       <main>
-        <Sidebar />
-        <Content />
+        <Sidebar projects={manageProject.projects} onAdd={showForm} />
+        <section>{showed}</section>
       </main>
     </>
   );
