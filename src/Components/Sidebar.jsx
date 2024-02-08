f00453dd0495ac85093915f8b365c61a2aa4c87d
Isaia Barzoi <isaiabarzoi@gmail.com>
2024-02-08 15:54:13 +0100

    styling sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,13 @@
+import Button from "./Button";
+import classes from "./Sidebar.module.css";
+
 export default function Sidebar() {
   return (
     <>
-      <aside></aside>
+      <aside className={classes.sidebar}>
+        <h2>Your Projects</h2>
+        <Button />
+      </aside>
     </>
   );
 }
